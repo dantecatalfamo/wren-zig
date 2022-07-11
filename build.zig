2eef83fbd1ddaf15c3949d6866a6adb7c52f1cbd
dantecatalfamo <dante.catalfamo@gmail.com>
2022-07-11 10:52:07 -0400

    Make it easier to include wren-zig

--- build.zig
+++ build.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const path = std.fs.path;
 
 pub fn build(b: *std.build.Builder) void {
     // Standard target options allows the person running `zig build` to choose
@@ -11,11 +12,7 @@ pub fn build(b: *std.build.Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const submodule_step = b.step("submodule", "Pull in git submodule");
-    submodule_step.makeFn = fetchSubmodule;
-
     const exe = b.addExecutable("wren-zig", "src/main.zig");
-    exe.step.dependOn(submodule_step);
     addWren(exe);
     exe.setTarget(target);
     exe.setBuildMode(mode);
@@ -31,7 +28,6 @@ pub fn build(b: *std.build.Builder) void {
     run_step.dependOn(&run_cmd.step);
 
     const exe_tests = b.addTest("src/main.zig");
-    exe_tests.step.dependOn(submodule_step);
     addWren(exe_tests);
     exe_tests.setTarget(target);
     exe_tests.setBuildMode(mode);
@@ -40,30 +36,39 @@ pub fn build(b: *std.build.Builder) void {
     test_step.dependOn(&exe_tests.step);
 }
 
-pub fn addWren(self: *std.build.LibExeObjStep) void {
-    self.addIncludePath("wren/src/include");
-    self.addIncludePath("wren/src/vm");
-    self.addIncludePath("wren/src/optional");
-    self.addCSourceFiles(&c_files, &.{});
-    self.linkSystemLibrary("m");
+pub fn addWren(exe: *std.build.LibExeObjStep) void {
+    var allocator = exe.builder.allocator;
+    const src_path = path.dirname(@src().file) orelse ".";
+    const include_path = path.join(allocator, &.{ src_path, "wren", "src", "include" }) catch unreachable;
+    const vm_path = path.join(allocator, &.{ src_path, "wren", "src", "vm" }) catch unreachable;
+    const optional_path = path.join(allocator, &.{ src_path, "wren", "src", "optional" }) catch unreachable;
+    exe.addIncludePath(include_path);
+    exe.addIncludePath(vm_path);
+    exe.addIncludePath(optional_path);
+    exe.linkSystemLibrary("m");
+
+    for (vm_c_files) |vm_c_file| {
+        const c_path = path.join(allocator, &.{ vm_path, vm_c_file }) catch unreachable;
+        exe.addCSourceFile(c_path, &.{});
+    }
+
+    for (optional_c_files) |opt_c_file| {
+        const c_path = path.join(allocator, &.{ optional_path, opt_c_file }) catch unreachable;
+        exe.addCSourceFile(c_path, &.{});
+    }
 }
 
-pub fn fetchSubmodule(self: *std.build.Step) !void {
-    _ = self;
-    var allocator = std.heap.page_allocator;
-    var process = try std.ChildProcess.init(&.{"git", "submodule", "update", "--init"}, allocator);
-    defer process.deinit();
-    _ = try process.spawnAndWait();
-}
+const optional_c_files = [_][]const u8 {
+    "wren_opt_meta.c",
+    "wren_opt_random.c",
+};
 
-const c_files = [_][]const u8 {
-    "wren/src/optional/wren_opt_meta.c",
-    "wren/src/optional/wren_opt_random.c",
-    "wren/src/vm/wren_compiler.c",
-    "wren/src/vm/wren_core.c",
-    "wren/src/vm/wren_debug.c",
-    "wren/src/vm/wren_primitive.c",
-    "wren/src/vm/wren_utils.c",
-    "wren/src/vm/wren_value.c",
-    "wren/src/vm/wren_vm.c",
+const vm_c_files = [_][]const u8 {
+    "wren_compiler.c",
+    "wren_core.c",
+    "wren_debug.c",
+    "wren_primitive.c",
+    "wren_utils.c",
+    "wren_value.c",
+    "wren_vm.c",
 };
